ae96ed34ed1953e3d0016fd0c09234e89da9b222
Christopher Kormanyos <ckormanyos@yahoo.com>
2021-04-03 23:01:25 +0200

    Remove no-longer-present files from CI build(s)

--- test/test.cpp
+++ test/test.cpp
@@ -3,7 +3,7 @@
 
 // Build locally for test on MinGW or Strawberry via Git bash.
 // cd C:\Users\User\Documents\Ks\PC_Software\NumericalPrograms\ExtendedNumberTypes\soft_double
-// g++ -finline-functions -finline-limit=64 -march=native -mtune=native -O3 -Wall -Wextra -pedantic -Wno-strict-aliasing -std=c++14 -I. math/softfloat-3/source/round_to_integral_types.cpp math/softfloat-3/source/s_addMagsF64.cpp math/softfloat-3/source/s_approxRecipSqrt32_1.cpp math/softfloat-3/source/s_normRoundPackToF64.cpp math/softfloat-3/source/s_roundPackToF64.cpp math/softfloat-3/source/s_subMagsF64.cpp test/test.cpp test/test_soft_double.cpp test/test_soft_double_with_decwide_t_100k_digit_pi.cpp -o test_soft_double.exe
+// g++ -finline-functions -finline-limit=64 -march=native -mtune=native -O3 -Wall -Wextra -pedantic -Wno-strict-aliasing -std=c++14 -I. test/test.cpp test/test_soft_double.cpp test/test_soft_double_with_decwide_t_100k_digit_pi.cpp -o test_soft_double.exe
 
 extern bool test_soft_double();
 extern bool test_soft_double_with_decwide_t_100k_digit_pi();
