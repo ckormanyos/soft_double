2afb803f434bc2b549d82839496c57870377a7c2
Christopher Kormanyos <ckormanyos@yahoo.com>
2022-09-21 09:54:08 +0200

    Add forgotten spot values file

---
+++ test/test_soft_double_spot_values.cpp
@@ -0,0 +1,20 @@
+///////////////////////////////////////////////////////////////////
+//  Copyright Christopher Kormanyos 2022.                        //
+//  Distributed under the Boost Software License,                //
+//  Version 1.0. (See accompanying file LICENSE_1_0.txt          //
+//  or copy at http://www.boost.org/LICENSE_1_0.txt)             //
+///////////////////////////////////////////////////////////////////
+
+#include <iomanip>
+#include <iostream>
+
+#include <math/softfloat/soft_double_examples.h>
+
+auto test_soft_double_spot_values() -> bool
+{
+  bool result_spot_values_is_ok = true;
+
+  std::cout << "Start test_soft_double_spot_values()" << std::endl;
+
+  return result_spot_values_is_ok;
+}
