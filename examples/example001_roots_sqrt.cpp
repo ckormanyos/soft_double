ec9588fa8831c85ba8816cf0d3924f1274ed2980
ckormanyos <ckormanyos@yahoo.com>
2025-02-27 19:32:24 +0100

    Try upgrade QEMU runs

--- examples/example001_roots_sqrt.cpp
+++ examples/example001_roots_sqrt.cpp
@@ -1,12 +1,11 @@
 ///////////////////////////////////////////////////////////////////
-//  Copyright Christopher Kormanyos 2020 - 2022.                 //
+//  Copyright Christopher Kormanyos 2020 - 2025.                 //
 //  Distributed under the Boost Software License,                //
 //  Version 1.0. (See accompanying file LICENSE_1_0.txt          //
 //  or copy at http://www.boost.org/LICENSE_1_0.txt)             //
 ///////////////////////////////////////////////////////////////////
 
 #include <cmath>
-#include <cstdint>
 
 #include <math/softfloat/soft_double.h>
 #include <math/softfloat/soft_double_examples.h>
